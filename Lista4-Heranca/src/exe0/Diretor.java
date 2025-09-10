0988de153d9ccea4d2e47a0fd0672362302dcf41
arthurkitice <art.ferreira301@gmail.com>
2025-09-10 08:39:58 -0300

    Conclusão da lista 4

--- Lista4-Heranca/src/exe0/Diretor.java
+++ Lista4-Heranca/src/exe0/Diretor.java
@@ -1,4 +1,34 @@
 package exe0;
 
-public class Diretor {
+public class Diretor extends Funcionario{
+    private double acoesEmpresa;
+
+    public Diretor() {
+        super();
+    }
+
+    public Diretor(String nome, String endereco, String cpf, double salario, double acoesEmpresa) {
+        super(nome, endereco, cpf, salario);
+        this.acoesEmpresa = acoesEmpresa;
+    }
+
+    public double getAcoesEmpresa() {
+        return acoesEmpresa;
+    }
+
+    public void setAcoesEmpresa(double acoesEmpresa) {
+        this.acoesEmpresa = acoesEmpresa;
+    }
+
+    @Override
+    public double calculaSalario(){
+        return super.calculaSalario() + this.acoesEmpresa * 2000;
+    }
+
+    @Override
+    public String toString() {
+        return "Diretor{" +
+                super.toString() + "acoesEmpresa=" + acoesEmpresa +
+                '}';
+    }
 }
--- Lista4-Heranca/src/exe0/Gerente.java
+++ Lista4-Heranca/src/exe0/Gerente.java
@@ -1,4 +1,34 @@
 package exe0;
 
-public class Gerente {
+public class Gerente extends Funcionario{
+    private double bonus;
+
+    public Gerente() {
+        super();
+    }
+
+    public Gerente(String nome, String endereco, String cpf, double salario, double bonus) {
+        super(nome, endereco, cpf, salario);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculaSalario(){
+        return super.calculaSalario() + this.bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                super.toString() + "bonus=" + bonus +
+                '}';
+    }
 }
