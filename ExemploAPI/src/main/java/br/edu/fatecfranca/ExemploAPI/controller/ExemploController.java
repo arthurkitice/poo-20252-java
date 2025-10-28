82ab3763ac51872fb962ecd3a5922cc9c79d052f
arthurkitice <art.ferreira301@gmail.com>
2025-10-28 11:14:24 -0300

    (28/10) Início do projeto API com banco de dados Postgres

--- ExemploAPI/src/main/java/br/edu/fatecfranca/ExemploAPI/controller/ExemploController.java
+++ ExemploAPI/src/main/java/br/edu/fatecfranca/ExemploAPI/controller/ExemploController.java
@@ -7,7 +7,7 @@ import br.edu.fatecfranca.ExemploAPI.model.Produto;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController //Sempre que chegar um requisição essa classe que vai responder
+@RestController //Sempre que chegar uma requisição essa classe que vai responder
 @RequestMapping("/produto") //Classe vai responder pelas requisições "/mensagem"
 public class ExemploController {
     List<Produto> produtos = new ArrayList<Produto>();
@@ -38,13 +38,22 @@ public class ExemploController {
 
     //Verbo DELETE
     @DeleteMapping("/{id}")
-    public Produto remover(@PathVariable int id){
-        for (Produto produto: produtos){
-            if(produto.getId() == id){
-                produtos.remove(produto);
-                return produto;
+    public boolean remover(@PathVariable int id){
+        return produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    //Verbo PUT - atualiza um produto
+    @PutMapping("/{id}")
+    public boolean atualize(@PathVariable int id, @RequestBody Produto novoProduto){
+        //Percorre os produtos para atualizar
+        for(Produto produto: produtos){
+            if(produto.getId() == id) {
+                produto.setNome(novoProduto.getNome());
+                produto.setQtde(novoProduto.getQtde());
+                produto.setPreco(novoProduto.getPreco());
+                return true;
             }
         }
-        return null;
+        return false;
     }
 }
