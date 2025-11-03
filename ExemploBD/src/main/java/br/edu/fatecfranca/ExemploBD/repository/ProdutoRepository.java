7c36997551d642d9a139772d96f8fc3f70b33277
arthurkitice <art.ferreira301@gmail.com>
2025-11-03 09:10:11 -0300

    (3/11) Continuação API com BD Postgres

---
+++ ExemploBD/src/main/java/br/edu/fatecfranca/ExemploBD/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.edu.fatecfranca.ExemploBD.repository;
+
+import br.edu.fatecfranca.ExemploBD.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    // Esta interface herdará todos os métodos de crud prontos da classe Produto
+    // Long indica tipo de dado da chave primária de Produto
+}
